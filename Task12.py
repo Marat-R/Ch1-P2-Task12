63bf5faad8f0b1af0ea3460f4058eab989e19aa3
Marat-R <you@example.com>
2019-12-21 12:59:39 +0600

    first commit

---
+++ Task12.py
@@ -0,0 +1,8 @@
+word = input("Input word: ")
+if "f" in word:
+    if word.count("f") > 1:
+        print(word.find("f"))
+        print(word.rfind("f")) 
+    else:
+        print(word.find("f"))
+
